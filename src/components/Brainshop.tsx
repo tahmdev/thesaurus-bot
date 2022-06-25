f822e25775901774c6a057966a2cbb3ae93d22f7
tahmdev <qwertiimusic@gmail.com>
2022-06-25 16:07:06 +0200

    Added Brainshop component

---
+++ src/components/Brainshop.tsx
@@ -0,0 +1,30 @@
+import React, { Dispatch, SetStateAction, useContext, useEffect } from "react";
+import { UserContext } from "../App";
+import { Message } from "./Chatwindow";
+
+interface Props {
+  messages: Message[] | null;
+  setMessages: Dispatch<SetStateAction<Message[] | null>>;
+}
+export const Brainshop: React.FC<Props> = ({ messages, setMessages }) => {
+  const uid = useContext(UserContext);
+  useEffect(() => {
+    if (messages?.at(-1)?.author === "user") {
+      fetch(
+        `http://localhost:9000/brainshop/${uid}/${messages?.at(-1)?.content}`,
+        {
+          method: "GET",
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          setMessages((prev) =>
+            prev
+              ? [...prev, { content: json.cnt, author: "bot" }]
+              : [{ content: json.cnt, author: "bot" }]
+          );
+        });
+    }
+  }, [messages]);
+  return null;
+};
--- src/components/Chatwindow.tsx
+++ src/components/Chatwindow.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Brainshop } from "./Brainshop";
 import { ChatInput } from "./ChatInput";
 import { MessageHistory } from "./Messagehistory";
 
@@ -13,6 +14,7 @@ export const Chatwindow: React.FC<Props> = () => {
     <div className="chatwindow-wrapper">
       <MessageHistory messages={messages} />
       <ChatInput setMessages={setMessages} />
+      <Brainshop messages={messages} setMessages={setMessages} />
     </div>
   );
 };
