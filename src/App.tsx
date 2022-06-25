abc961bfa23b9a193ff5495a92f0f8fdca5ec827
tahmdev <qwertiimusic@gmail.com>
2022-06-25 16:06:55 +0200

    Added temporary userID

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Chatwindow } from "./components/Chatwindow";
+
+const uid = Math.random().toString(36).slice(2);
+export const UserContext = React.createContext(uid);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UserContext.Provider value={uid}>
+        <Chatwindow />
+      </UserContext.Provider>
+    </div>
+  );
 }
 
 export default App;
